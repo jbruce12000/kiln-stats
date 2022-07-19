ff1adb44e38a094a4ed2aa56d2bbc7ad782205c0
jbruce <jason.bruce@coxautoinc.com>
2022-07-19 08:43:02 -0400

    adding a script to split logs up

---
+++ scripts/log-splitter.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -w
+use strict;
+
+# This script splits a log up by adding a number
+# at the start of each line.
+
+my $x=0;
+my $record=0;
+
+while(<>) {
+
+  if($_=~/schedule ended, shutting down/) {
+    print "$x $_";
+    $record=0;
+    }
+
+  if($_=~/cmd...RUN/) {
+    $x++;
+    $record=1;
+    }
+
+  if($record) {
+    print "$x $_";
+    }
+  }
