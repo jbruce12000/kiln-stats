bce995dbccc61e93594c3a5143d3dd291b53ced8
jbruce <jbruce12000@gmail.com>
2022-11-05 12:06:52 -0400

    remove temps with error over 500

--- scripts/go.sql
+++ scripts/go.sql
@@ -8,10 +8,12 @@ create table out_of_window as select run_time,count(*) from kiln group by run_ti
 .output ../tmp/reports.txt
 select * from kiln a, out_of_window b where a.run_time=b.run_time;
 
-# remove the first and last log entry
+# remove the first and last log entry and crazy entries
 delete from kiln where run_time=0;
 delete from kiln where time_left=0;
 update kiln set d=0 where abs(d)>100;
+delete from kiln where error/1.0 > 500;
+delete from kiln where error/1.0 < -500;
 
 # invert error
 update kiln set error=error*-1.0;
